12d0bf06031c2862b7d96676d850fda5cd8a5a06
Andrew Hockman <spamdrew128@gmail.com>
2024-05-27 02:14:59 -0400

    updated searchmanager

--- src/search/search_manager.rs
+++ src/search/search_manager.rs
@@ -8,13 +8,27 @@ const MAX_DEPTH: Depth = i8::MAX;
 pub const MAX_PLY: Ply = MAX_DEPTH as u8;
 
 pub struct SearchManager {
+    searcher: Searcher,
     board: Board,
 }
 
 impl SearchManager {
     pub fn new() -> Self {
         Self {
+            searcher: Searcher::new(),
             board: Board::from_fen(START_FEN),
         }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Searcher {
+    // info
+    seldepth: u8,
+}
+
+impl Searcher {
+    fn new() -> Self {
+        Self { seldepth: 0 }
     }
 }
