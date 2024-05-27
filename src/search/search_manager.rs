264aee2ebadfa5fdeaf2546427beb9b5a2b50eb6
Andrew Hockman <spamdrew128@gmail.com>
2024-05-26 22:10:28 -0400

    omw

--- src/search/search_manager.rs
+++ src/search/search_manager.rs
@@ -1,13 +1,13 @@
-use crate::movegen::board_rep::Board;
+use crate::movegen::board_rep::{Board, START_FEN};
 
 pub struct SearchManager {
     board: Board,
 }
 
 impl SearchManager {
-    pub fn new(fen: &str) -> Self {
+    pub fn new() -> Self {
         Self {
-            board: Board::from_fen(fen),
+            board: Board::from_fen(START_FEN),
         }
     }
 }
--- src/uci/uci_handler.rs
+++ src/uci/uci_handler.rs
@@ -1,13 +1,28 @@
-use crate::{movegen::board_rep::START_FEN, search::search_manager::SearchManager};
+use crate::search::search_manager::SearchManager;
+
+enum Token {
+    
+}
 
 pub struct UciHandler {
     search_manager: SearchManager,
 }
 
+fn end_of_transmission(buffer: &str) -> bool {
+    buffer
+        .chars()
+        .next()
+        .map_or(false, |c| c == char::from(0x04))
+}
+
+fn kill_program() {
+    std::process::exit(0);
+}
+
 impl UciHandler {
     pub fn new() -> Self {
         Self {
-            search_manager: SearchManager::new(START_FEN)
+            search_manager: SearchManager::new()
         }
     }
 }
