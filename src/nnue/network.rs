5f168c716214740df85b472cd61197175a06e65e
Andrew Hockman <spamdrew128@gmail.com>
2024-06-03 20:04:27 -0400

    init net

--- src/nnue/network.rs
+++ src/nnue/network.rs
@@ -1,4 +1,4 @@
-use crate::movegen::board_rep::{Color, Piece, Square};
+use crate::movegen::board_rep::{Board, Color, Piece, Square};
 
 const INPUT_SIZE: usize = Square::CNT as usize * Piece::CNT as usize * Color::CNT as usize;
 const L1_SIZE: usize = 64;
@@ -7,11 +7,6 @@ const OUTPUT_SCALE: i16 = 64;
 
 static NNUE: Network =
     unsafe { std::mem::transmute(*include_bytes!(concat!(env!("OUT_DIR"), "/net.bin"))) };
-
-#[repr(C, align(64))]
-pub struct Accumulator {
-    vals: [i16; L1_SIZE],
-}
 
 #[repr(C, align(64))]
 pub struct L1Params {
@@ -26,13 +21,7 @@ pub struct Network {
     output_bias: i16,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::{Network, NNUE};
-
-    #[test]
-    fn hi() {
-        let h = &NNUE;
-        print!("i");
-    }
+#[repr(C, align(64))]
+pub struct Accumulator {
+    vals: [[i16; L1_SIZE]; Color::CNT as usize],
 }
