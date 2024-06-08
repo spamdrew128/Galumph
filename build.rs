17890a32f67623cdf3b7db1d06c1b5671a0a8874
Andrew Hockman <spamdrew128@gmail.com>
2024-06-08 15:01:48 -0400

    fixing things up

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ use build_script_stuff::magic_builder::get_magic_bytes;
 use build_script_stuff::nnue_bin_encoder::get_random_nnue_bytes;
 
 use std::fs::File;
+use std::fs::ReadDir;
 use std::io::BufWriter;
 use std::io::Write;
 use std::path::PathBuf;
@@ -28,24 +29,63 @@ fn get_existing_net_bytes() -> Vec<u8> {
     vec![]
 }
 
-fn copy_file(folder: &str, name: &str) -> Result<u64, std::io::Error> {
-    let mut from = std::env::current_dir().unwrap();
-    from.push(folder);
-    from.push(name);
-
+fn copy_file(from_path: PathBuf, name: &str) -> Result<u64, std::io::Error> {
     let mut out_dir: PathBuf = std::env::var("OUT_DIR").unwrap().into();
     out_dir.push(name);
 
-
-    std::fs::copy(from, out_dir)
+    std::fs::copy(from_path, out_dir)
 }
 
-fn copy_header() {
-    copy_file("net_binary", "header.rs").expect("HEADER IS MISSING!!!!");
-}
+fn copy_net_to_out_dir() {
+    fn try_from_dir(dir_paths: &mut ReadDir) -> bool {
+        while let Some(Ok(dir_entry)) = dir_paths.next() {
+            let name = dir_entry
+                .file_name()
+                .into_string()
+                .expect("INVALID FILE NAME");
+            let mut path = dir_entry.path();
 
-fn copy_net() {
-    copy_file("net_binary", "header.rs").expect("HEADER IS MISSING!!!!");
+            if let Ok(bytes_read) = copy_file(path.clone(), name.as_str()) {
+                if bytes_read == 0 {
+                    continue;
+                }
+
+                path.pop();
+                path.push("header.rs");
+                copy_file(path, "header.rs").expect("HEADER INVALID");
+
+                return true;
+            }
+        }
+
+        false
+    }
+
+    // try from user net directory
+    let mut dir = std::env::current_dir().unwrap();
+    dir.push("net_binaries");
+    dir.push("user");
+
+    let dir_paths = std::fs::read_dir(dir.clone());
+
+    if let Ok(mut paths) = dir_paths {
+        if try_from_dir(&mut paths) {
+            return;
+        }
+    }
+
+    // since user was empty, try from default directory
+    dir.pop();
+    dir.push("default");
+    let dir_paths = std::fs::read_dir(dir);
+
+    if let Ok(mut paths) = dir_paths {
+        if try_from_dir(&mut paths) {
+            return;
+        }
+    }
+
+    panic!("NO VALID NET FILES FOUND!");
 }
 
 fn main() {
@@ -58,12 +98,10 @@ fn main() {
     // NNUE file generation
     let existing_nnue_bytes = get_existing_net_bytes();
 
-    if existing_nnue_bytes.is_empty() {
-        let nnue_bytes = get_random_nnue_bytes();
-        gen_output_file("net.bin", nnue_bytes.bytes.as_slice());
-    } else {
-        gen_output_file("net.bin", existing_nnue_bytes.as_slice());
-    }
-
-    copy_header();
+    // if existing_nnue_bytes.is_empty() {
+    //     let nnue_bytes = get_random_nnue_bytes();
+    //     gen_output_file("net.bin", nnue_bytes.bytes.as_slice());
+    // } else {
+    //     gen_output_file("net.bin", existing_nnue_bytes.as_slice());
+    // }
 }
